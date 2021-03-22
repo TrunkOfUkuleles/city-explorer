e5d611c2a478e753fcf2227c70624d02f92decef
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-03-22 16:28:57 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <p>hello</p>
+        <h1>hellos</h1>
       </header>
     </div>
   );
