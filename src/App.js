7d6c01a0123898f93043c4fc180febd8a0def736
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-03-23 01:17:43 -0700

    fff
    c

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import axios from 'axios';
 
@@ -19,18 +18,18 @@ import Form from 'react-bootstrap/Form';
 
   getTest = async(e) => {
     e.preventDefault();
-    const url=`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=Seattle&format='json'`;
+    const url=`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.quer}&format=json`;
     const base = await axios.get(url);
-    const one = base.data;
-    console.log('one: ', {one})
+    const onee = base.data;
+    console.log('onee: ', {onee})
     this.setState({
-      location: one[0],
-      pop: true,
-      ll: [one[0].lat , one[0].lon],
-      // imgsrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${one[0].lat},${one[0].lon}&zoom=13`,
+      location: onee[0],
+      displayResults: true,
+      ll: [onee[0].lat , onee[0].lon],
+      imgsrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${onee[0].lat},${onee[0].lon}&zoom=13`
     });
-    console.log(this.state.ll)
-    console.log('state: ', this.state.one)
+    console.log(this.state.ll) 
+    console.log('state: ', this.state.onee)
 
   }
   
@@ -40,16 +39,16 @@ import Form from 'react-bootstrap/Form';
   return (    
    <>
    
-          <h1 backgroundColor="red"> {this.state.location.display_name || 'TEMP'} </h1>
+          <h1> {this.state.location.display_name || 'TEMP'} </h1>
           <Form onSubmit={this.getTest}>
-            <input onChange={(e) => this.setState({quer: e.target.value})} type="text" placeholder="Enter your favorite city!" />
-          <button type="submit"> "Hello" </button>
+            <input onChange={(e) => this.setState({quer: e.target.value})} placeholder="Enter your favorite city!" />
+          <button type="submit"> Hello </button>
           </Form>
       
         {this.state.displayResults  && 
         <>
           <p> lat: {this.state.ll[0]}, lon: {this.state.ll[1]} </p>
-           {/* <img src={this.state.imgsrc} alt='blah' /> */}
+           <img src={this.state.imgsrc} alt='blah' />
         </>
       }  
       </>   
