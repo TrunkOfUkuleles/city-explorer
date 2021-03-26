a212e5278137fb66cb376786b2c65b5013961716
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-03-25 20:43:56 -0700

    w

--- getLocation.js
+++ getLocation.js
@@ -2,8 +2,13 @@
 'use strict';
 
 
-getLocation = async(e) => {
+ async function getLocation(e) {
     console.log(this.state)
+
+
+
+
+
     try {
       e.preventDefault();
       const url = `https://us1.locationiq.com/v1/search.php?q=${this.state.quer}&format=json&key=${process.env.REACT_APP_LOCATION_KEY}`;
@@ -14,12 +19,13 @@ getLocation = async(e) => {
         location: locationArray[0],
         displayResults: true,
         imgsrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=13`,
-        
+        loclat: locationArray[0].lat,
+        loclon: locationArray[0].lon
       });
 
-
     } catch (error) {
-    
       <Error error={error.message} />;
     }
   };
+
+modular.exports = getLocation;
--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-'use strict';
 
 import React from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Card from "react-bootstrap/Card";
-import Forecast from "./forecast.js";
-import Error from "./error.js";
-import getWeather from './getWeather'
+import Card from 'react-bootstrap/Card'
 
+import Form from 'react-bootstrap/Form';
+import Error from './error';
+require('dotenv').config();
 
 class App extends React.Component {
   constructor(props) {
@@ -24,66 +21,77 @@ class App extends React.Component {
       forc: [],
       showForc: false,
       weather:{},
-      latlon: {},
-      showMyForc: false,
+      loclat:'',
+      loclon: '',
     };
   }
 
-  renderForcasts = () => {
-    await axios.get(process.env.REACT_APP_LOCAL_KEY2, {params: this.state.latlon})
-      .then((rees) => {this.setState({
-        weather: rees.data,
-        showMyForc: true,
-        showForc: false,
+ 
+
+
+
+     getLocation = (e) => {
+      e.preventDefault();
+      const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.quer}&format=json`;
+       axios.get(url)
+      .then((local) => {
+        const matches = local.data
+        console.log(local, 'string')
+      this.setState({ location: matches[0],  loclat: matches[0].lat , loclon: matches[0].lon, displayResults: true, 
+        imgsrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${matches[0].lat},${matches[0].lon}`
+      
       })})
       .catch((error) => {
-      <Error error={error.message} />;
+        <Error error={error.message} />;
+      })
+    }
+  
+    getWeath = (e) => {
+      e.preventDefault();
+      const url = process.env.REACT_APP_LOCAL_SERV;
+      const q = {
+        lat: this.state.loclat,
+        lon: this.state.loclon,
+    }
+
+     axios.get(url)
+     .query(q)
+      .then((forecasts) => {
+        const matcher = forecasts.data
+        this.setState({ weather: matcher,
+                        showForc: true})
+      })
+      .catch((error) => {
+        <Error error={error.message} />
       })
     }
   
 
   render() {
+
     return (
       <>
-      {<h1>{this.state.location.display_name} || "Welcome"</h1>}
+      <h1> Welcome</h1>
 
-        <Form class='top-box' onSubmit={getWeather}>
-          <input
-            onChange={(e) => this.setState({ quer: e.target.value })}
-            placeholder="Enter your favorite city!"
-          />
-          <Button type="submit"> Explore! </Button>
+        <Form className='top-box' onSubmit={this.getLocation}>
+          <input onChange={(e) => this.setState({ quer: e.target.value })}
+            placeholder="Enter your favorite city!" />
+          <button type="submit"> Explore! </button>
         </Form>
-
-  {       this.state.displayResults &&  
-  <>
-          <Card class='location-base'>
-            <Card.Img variant='top' src={this.state.imgsrc} alt="map" rounded />
-            <Card.Body>
-            <Card.Title>{this.state.location.display_name}</Card.Title>
-            <Card.Text>
-              lat: {this.state.location.lat}, 
-              lon: {this.state.location.lon},
-              weather: {this.state.weather}
-            </Card.Text>
-            </Card.Body>
-          </Card>
+        <Card className='location-base'>
+              <Card.Img variant='top' src={this.state.imgsrc} alt="map" rounded />
+              <Card.Body>
           
-        {this.state.showMyForc &&
-         <Button onClick={this.foreRend}>Forcast</Button>}
-         </>
-         }
-        {
-         this.state.showForc ?
-           <Card>
-            <Forecast data={this.state.forc} />
-          </Card> 
-          :
-          <Button onClick={this.toggleForc}>See some Forecasts</Button>
-  }
-    </>
-    );
-  }
+              <Card.Text>
+                lat: {this.state.loclat}, 
+                lon: {this.state.loclon},
+              </Card.Text>
+              </Card.Body>
+            </Card>
+  
+              <button onClick={this.getWeath}>Get Some Forecasts</button>
+ 
+  </>)
 }
-
-export default App;
+}
+export default App
